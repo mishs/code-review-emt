0c41f010e76ccc825e986e82c959a029ef2233af
Mish <mishuct26@gmail.com>
2021-07-13 22:21:01 +0200

    Refactoring styled-components

--- src/components/DevicesHeader.js
+++ src/components/DevicesHeader.js
@@ -3,11 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { Button } from 'react-bootstrap'
 import Search from './Search'
-import styled from 'styled-components'
 
 function DevicesHeader() {
     return (
-        <Styles>
+        <div className="devices-header">
             <div className="title">
                 <FontAwesomeIcon icon={faHome} />
                 <h3>Device Overview</h3>
@@ -19,44 +18,9 @@ function DevicesHeader() {
                 <FontAwesomeIcon icon={faPlus} />
                 add
             </Button>
-        </Styles>
+        </div>
     )
 }
 
-const Styles = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-
-    h3 {
-        font-size: 1rem;
-        color: #7B7B7B;
-        margin-left: 5px;
-        margin-bottom: 0;
-    }
-
-    .title {
-        display: flex;
-        align-items: center;
-        margin-left: 2rem;
-
-        svg {
-            color: #7B7B7B;
-        }
-    }
-
-    button {
-        background: #fff;
-        border: 1px solid #BBBBBB;
-        border-radius: 3px;
-        color: #2A94E3;
-        padding: 3px 6px;
-
-        svg {
-            margin-right: 5px;
-        }
-    }
-`
 
 export default DevicesHeader
